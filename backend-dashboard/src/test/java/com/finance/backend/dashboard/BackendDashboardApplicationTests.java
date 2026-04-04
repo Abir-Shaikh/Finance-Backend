7deb77483e6fb5f797789f231acd55ba2090423d
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-04 22:09:14 +0530

    Initial commit

---
+++ backend-dashboard/src/test/java/com/finance/backend/dashboard/BackendDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.backend.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

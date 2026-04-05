c7b0be32c355abd74dc2c1b31acc4e4ec3e8f157
Abir-Shaikh <shaikhabir044@gmail.com>
2026-04-05 09:46:23 +0530

    Repository logic added

--- backend-dashboard/src/main/java/com/finance/backend/dashboard/repository/RecordRepository.java
+++ backend-dashboard/src/main/java/com/finance/backend/dashboard/repository/RecordRepository.java
@@ -1,4 +1,12 @@
 package com.finance.backend.dashboard.repository;
 
-public class RecordRepository {
+import com.finance.backend.dashboard.model.FinancialRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RecordRepository extends JpaRepository<FinancialRecord , Long> {
+    List<FinancialRecord> findByType(String type);
+    List<FinancialRecord> findByCategory(String category);
+    List<FinancialRecord> findByTypeAndCategory(String type, String category);
 }
--- backend-dashboard/src/main/java/com/finance/backend/dashboard/repository/UserRepository.java
+++ backend-dashboard/src/main/java/com/finance/backend/dashboard/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.finance.backend.dashboard.repository;
 
-public class UserRepository {
+import com.finance.backend.dashboard.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User , Long> {
+    boolean existsByEmail(@NotBlank(message = "Email cannot be blank") @Email(message = "Invalid Email Format") String email);
+
+    Optional<User> findByEmail(String email);
 }
